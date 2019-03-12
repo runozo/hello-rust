099bf7d6c4fa8d1bbfc6c1c532cf3b98ebdff158
Cesare Ghirelli <cghirelli@arpae.it>
2019-03-12 12:01:46 +0100

    arrays explained

---
+++ src/arrays.rs
@@ -0,0 +1,24 @@
+// Arrays - Fixed list where elements are the same data types
+use std::mem;
+
+pub fn run() {
+    let mut numbers: [i32; 5] = [1,2,3,4,5];
+
+    println!("{:?}", numbers);
+    
+    // Re-assign value
+    numbers[2] = 20;
+    
+    // Get single value
+    println!("Single value: {}", numbers[0]);
+
+    // Get array length
+    println!("Array Length: {}", numbers.len());
+
+    // Array are stack allocated
+    println!("Array occupies {} bytes", mem::size_of_val(&numbers));
+
+    // Get Slice
+    let slice: &[i32] = &numbers[0..2];
+    println!("Slice: {:?}", slice);
+}
